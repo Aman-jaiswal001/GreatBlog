e855a33bec2c79778d668ce456355da10c7db4f4
Aman Jaiswal <aman9682037@gmail.com>
2026-07-13 15:28:04 +0530

    removing extra conole

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -47,7 +47,6 @@ export const addBlog = async (req, res)=> {
         res.json({success: true, message : "Blog added successfully"})
     } catch (error) {
         console.log("error return")
-        console.log(error.message)
         res.json({success: false, message: error.message})   
     }
 
